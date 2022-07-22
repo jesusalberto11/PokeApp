fff0cd4a4bf8bbeeacfd69e14a651d39081d3ef3
Jesus Morales <jesusalbertomoralesrico@gmail.com>
2022-07-21 19:38:34 -0500

    Feature: Created Index Page

---
+++ src/pages/IndexPage.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+const IndexPage = () => {
+  return (
+    <View>
+      <Text>Hello world from index!</Text>
+    </View>
+  );
+};
+
+export default IndexPage;
