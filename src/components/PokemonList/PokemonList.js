1ec9eff962bbece85a51f9078935bce7c60a95bc
Jesus Morales <jesusalbertomoralesrico@gmail.com>
2022-07-24 17:41:11 -0500

    Refractor: Added pokemon component to render item function

--- src/components/PokemonList/PokemonList.js
+++ src/components/PokemonList/PokemonList.js
@@ -1,36 +1,17 @@
 import React from "react";
-import {
-  StyleSheet,
-  View,
-  Text,
-  FlatList,
-  TouchableOpacity,
-} from "react-native";
+import { StyleSheet, View, FlatList, TouchableOpacity } from "react-native";
 import { AppStyles } from "../../config/Styles";
+import PokemonCard from "./PokemonCard";
 
 const PokemonList = ({ pokemons }) => {
   const renderItem = ({ item: pokemon }) => {
-    if (!pokemon) return;
     return (
       <TouchableOpacity
         onPress={() => {
           console.log(pokemon.id);
         }}
       >
-        <View style={styles.listItem}>
-          <View style={styles.pokemonImage}>
-            <img
-              src={pokemon?.sprites.front_default}
-              width={100}
-              height={100}
-              alt={pokemon?.name}
-            />
-          </View>
-          <View style={styles.pokemonDetails}>
-            <Text style={styles.pokemonName}>{pokemon?.name}</Text>
-            <Text>Type: {pokemon?.types[0].type.name}</Text>
-          </View>
-        </View>
+        <PokemonCard pokemon={pokemon} />
       </TouchableOpacity>
     );
   };
@@ -65,18 +46,6 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: "center",
     justifyContent: "center",
-  },
-  pokemonImage: {
-    flex: 2,
-    backgroundColor: "blue",
-  },
-  pokemonDetails: {
-    flex: 1,
-    backgroundColor: "red",
-  },
-  pokemonName: {
-    fontSize: 18,
-    fontWeight: "bold",
   },
 });
 
