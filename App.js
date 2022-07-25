61f1d9e47ebf058eabd6b14af7435bebca985bac
Jesus Morales <jesusalbertomoralesrico@gmail.com>
2022-07-24 20:28:52 -0500

    Feature: Added navigation stack and header component

--- App.js
+++ App.js
@@ -1,12 +1,49 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, SafeAreaView } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import IndexPage from "./src/pages/IndexPage";
+import PokemonPage from "./src/pages/PokemonPage";
+import { AppStyles } from "./src/config/Styles";
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
     <SafeAreaView style={styles.container}>
-      <IndexPage />
-      <StatusBar style="dark" />
+      <NavigationContainer>
+        <Stack.Navigator initialRouteName="Home">
+          <Stack.Screen
+            name="Home"
+            component={IndexPage}
+            options={{
+              title: "Pokedex",
+              headerStyle: {
+                backgroundColor: AppStyles.colors.headerColor,
+              },
+              headerTintColor: "#fff",
+              headerTitleStyle: {
+                fontWeight: "bold",
+              },
+            }}
+          />
+          <Stack.Screen
+            name="PokemonPage"
+            component={PokemonPage}
+            options={{
+              title: "Pokemon details",
+              headerStyle: {
+                backgroundColor: AppStyles.colors.headerColor,
+              },
+              headerTintColor: "#fff",
+              headerTitleStyle: {
+                fontWeight: "bold",
+              },
+            }}
+          />
+        </Stack.Navigator>
+        <StatusBar style="light" />
+      </NavigationContainer>
     </SafeAreaView>
   );
 }
