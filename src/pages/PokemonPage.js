88dbe37233bab407f20335386eeee2a90c11f850
Jesus Morales <jesusalbertomoralesrico@gmail.com>
2022-07-24 22:28:06 -0500

    Feature: Created pokemon page basic layout

--- src/pages/PokemonPage.js
+++ src/pages/PokemonPage.js
@@ -1,12 +1,41 @@
 import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, Image } from "react-native";
 
 const PokemonPage = ({ route }) => {
   const { pokemon } = route.params;
   return (
     <View style={styles.container}>
-      <Text>{pokemon.name} </Text>
-      <Text>Pokemon id: {pokemon.id}</Text>
+      <View style={styles.pokemonNameContainer}>
+        <Text style={styles.pokemonName}>{pokemon?.name}</Text>
+      </View>
+      <View style={styles.pokemonImageContainer}>
+        <Image
+          style={styles.pokemonImage}
+          source={{ uri: pokemon?.sprites.front_default }}
+        />
+      </View>
+      <View style={styles.PokemonDetailsContainer}>
+        <View style={styles.PokemonBaseStatsContainer}>
+          <Text style={{}}>Height: {pokemon?.height / 10} m</Text>
+          <Text>Weight: {pokemon?.weight / 10} kg</Text>
+          <Text>Base XP: {pokemon?.base_experience}</Text>
+        </View>
+        <Text>Species: {pokemon?.species.name}</Text>
+
+        {pokemon?.types.map((item, id) => {
+          return <Text key={id}>Types: {item.type.name}</Text>;
+        })}
+        {pokemon?.stats.map((item, id) => {
+          return (
+            <Text key={id}>
+              {item.stat.name} - {item.base_stat}
+            </Text>
+          );
+        })}
+        {pokemon?.abilities.map((item, id) => {
+          return <Text key={id}>{item.ability.name}</Text>;
+        })}
+      </View>
     </View>
   );
 };
@@ -14,6 +43,35 @@ const PokemonPage = ({ route }) => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    flexDirection: "column",
+  },
+  pokemonImageContainer: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  pokemonImage: {
+    flex: 1,
+    height: 20,
+    width: 200,
+  },
+  pokemonNameContainer: {
+    flex: 0.5,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  pokemonName: {
+    fontSize: 36,
+    fontWeight: "200",
+  },
+  PokemonDetailsContainer: {
+    flex: 3,
+  },
+  PokemonBaseStatsContainer: {
+    flexDirection: "row",
+    justifyContent: "space-evenly",
+    backgroundColor: "red",
   },
 });
+
 export default PokemonPage;
