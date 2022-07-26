5bb185a7cdea2d057f337dbedd1528b1e93721c6
Jesus Morales <jesusalbertomoralesrico@gmail.com>
2022-07-26 16:18:14 -0500

    Feature: Added Store context for all components

--- App.js
+++ App.js
@@ -2,6 +2,8 @@ import { StatusBar } from "expo-status-bar";
 import { StyleSheet, SafeAreaView } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { Provider } from "react-redux";
+import { store } from "./src/app/store";
 import IndexPage from "./src/pages/IndexPage";
 import PokemonPage from "./src/pages/PokemonPage";
 import { AppStyles } from "./src/config/Styles";
@@ -10,41 +12,43 @@ const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <SafeAreaView style={styles.container}>
-      <NavigationContainer>
-        <Stack.Navigator initialRouteName="Home">
-          <Stack.Screen
-            name="Home"
-            component={IndexPage}
-            options={{
-              title: "Pokedex",
-              headerStyle: {
-                backgroundColor: AppStyles.colors.headerColor,
-              },
-              headerTintColor: "#fff",
-              headerTitleStyle: {
-                fontWeight: "bold",
-              },
-            }}
-          />
-          <Stack.Screen
-            name="PokemonPage"
-            component={PokemonPage}
-            options={{
-              title: "Pokemon details",
-              headerStyle: {
-                backgroundColor: AppStyles.colors.headerColor,
-              },
-              headerTintColor: "#fff",
-              headerTitleStyle: {
-                fontWeight: "bold",
-              },
-            }}
-          />
-        </Stack.Navigator>
-        <StatusBar style="light" />
-      </NavigationContainer>
-    </SafeAreaView>
+    <Provider store={store}>
+      <SafeAreaView style={styles.container}>
+        <NavigationContainer>
+          <Stack.Navigator initialRouteName="Home">
+            <Stack.Screen
+              name="Home"
+              component={IndexPage}
+              options={{
+                title: "Pokedex",
+                headerStyle: {
+                  backgroundColor: AppStyles.colors.headerColor,
+                },
+                headerTintColor: "#fff",
+                headerTitleStyle: {
+                  fontWeight: "bold",
+                },
+              }}
+            />
+            <Stack.Screen
+              name="PokemonPage"
+              component={PokemonPage}
+              options={{
+                title: "Pokemon details",
+                headerStyle: {
+                  backgroundColor: AppStyles.colors.headerColor,
+                },
+                headerTintColor: "#fff",
+                headerTitleStyle: {
+                  fontWeight: "bold",
+                },
+              }}
+            />
+          </Stack.Navigator>
+          <StatusBar style="light" />
+        </NavigationContainer>
+      </SafeAreaView>
+    </Provider>
   );
 }
 
