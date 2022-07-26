f87abc6c311e4956dde48899cf8d03e015f95f60
Jesus Morales <jesusalbertomoralesrico@gmail.com>
2022-07-26 18:47:33 -0500

    Feature: created add, search and togle loading reducers

--- src/features/pokemons/PokemonSlice.js
+++ src/features/pokemons/PokemonSlice.js
@@ -1,15 +1,31 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, current } from "@reduxjs/toolkit";
 
 const initialState = {
   allPokemons: [],
+  currentPokemon: [],
   isLoading: true,
-  appName: "Eduardo",
 };
 
 export const pokemonSlice = createSlice({
   name: "pokemons",
   initialState,
-  reducers: {},
+  reducers: {
+    addPokemonToStore: (state, action) => {
+      state.allPokemons = [...state.allPokemons, action.payload];
+    },
+    getPokemonById: (state, action) => {
+      const pokemons = current(state.allPokemons);
+      let finded = pokemons.filter((pkm) => pkm.id === action.payload);
+
+      state.currentPokemon = finded[0];
+    },
+    setIsLoading: (state, action) => {
+      state.isLoading = action.payload;
+    },
+  },
 });
+
+export const { addPokemonToStore, getPokemonById, setIsLoading } =
+  pokemonSlice.actions;
 
 export default pokemonSlice.reducer;
