c68d8a27c42e93b6098259f58254d64ce003eb04
Jesus Morales <jesusalbertomoralesrico@gmail.com>
2022-07-26 19:10:58 -0500

    Refractor: Splitted pokemon details in components

--- src/pages/PokemonPage.js
+++ src/pages/PokemonPage.js
@@ -1,40 +1,27 @@
 import React from "react";
-import { View, Text, StyleSheet, Image } from "react-native";
+import { View, StyleSheet } from "react-native";
+import PokemonName from "../components/PokemonPage/PokemonName";
+import PokemonImage from "../components/PokemonPage/PokemonImage";
+import PokemonBaseStats from "../components/PokemonPage/PokemonBaseStats";
+import PokemonTypes from "../components/PokemonPage/PokemonTypes";
+import PokemonStats from "../components/PokemonPage/PokemonStats";
+import PokemonAbilities from "../components/PokemonPage/PokemonAbilities";
 
 const PokemonPage = ({ route }) => {
   const { pokemon } = route.params;
   return (
     <View style={styles.container}>
-      <View style={styles.pokemonNameContainer}>
-        <Text style={styles.pokemonName}>{pokemon?.name}</Text>
-      </View>
-      <View style={styles.pokemonImageContainer}>
-        <Image
-          style={styles.pokemonImage}
-          source={{ uri: pokemon?.sprites.front_default }}
-        />
-      </View>
+      <PokemonName name={pokemon?.name} />
+      <PokemonImage imageURL={pokemon?.sprites.front_default} />
       <View style={styles.PokemonDetailsContainer}>
-        <View style={styles.PokemonBaseStatsContainer}>
-          <Text style={{}}>Height: {pokemon?.height / 10} m</Text>
-          <Text>Weight: {pokemon?.weight / 10} kg</Text>
-          <Text>Base XP: {pokemon?.base_experience}</Text>
-        </View>
-        <Text>Species: {pokemon?.species.name}</Text>
-
-        {pokemon?.types.map((item, id) => {
-          return <Text key={id}>Types: {item.type.name}</Text>;
-        })}
-        {pokemon?.stats.map((item, id) => {
-          return (
-            <Text key={id}>
-              {item.stat.name} - {item.base_stat}
-            </Text>
-          );
-        })}
-        {pokemon?.abilities.map((item, id) => {
-          return <Text key={id}>{item.ability.name}</Text>;
-        })}
+        <PokemonBaseStats
+          height={pokemon?.height}
+          weight={pokemon?.weight}
+          baseXP={pokemon?.base_experience}
+        />
+        <PokemonTypes types={pokemon?.types} />
+        <PokemonStats stats={pokemon?.stats} />
+        <PokemonAbilities abilities={pokemon?.abilities} />
       </View>
     </View>
   );
@@ -45,32 +32,8 @@ const styles = StyleSheet.create({
     flex: 1,
     flexDirection: "column",
   },
-  pokemonImageContainer: {
-    flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  pokemonImage: {
-    flex: 1,
-    height: 20,
-    width: 200,
-  },
-  pokemonNameContainer: {
-    flex: 0.5,
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  pokemonName: {
-    fontSize: 36,
-    fontWeight: "200",
-  },
   PokemonDetailsContainer: {
     flex: 3,
-  },
-  PokemonBaseStatsContainer: {
-    flexDirection: "row",
-    justifyContent: "space-evenly",
-    backgroundColor: "red",
   },
 });
 
