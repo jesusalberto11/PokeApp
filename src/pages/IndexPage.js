9259ba4c09aa5a6fc02a1399beabb28c1db1ed0c
Jesus Morales <jesusalbertomoralesrico@gmail.com>
2022-07-28 17:57:36 -0500

    Refractor: Added buttons to handle pagination

--- src/pages/IndexPage.js
+++ src/pages/IndexPage.js
@@ -1,13 +1,12 @@
 import React, { useState } from "react";
 import { useEffect } from "react";
 import axios from "axios";
-import { StyleSheet, View, Text } from "react-native";
+import { StyleSheet, View, Text, Button } from "react-native";
 import { useSelector, useDispatch } from "react-redux";
 import {
   addPokemonToStore,
   setIsLoading,
-  setFetchingNewPokemonsStart,
-  setFetchingNewPokemonsEnded,
+  cleanPokemons,
 } from "../features/pokemons/PokemonSlice";
 import { AppStyles } from "../config/Styles";
 import PokemonList from "../components/PokemonList/PokemonList.js";
@@ -17,6 +16,7 @@ const IndexPage = ({ navigation }) => {
     "https://pokeapi.co/api/v2/pokemon?limit=25&offset=0"
   );
   const [nextURL, setNextURL] = useState("");
+  const [previousURL, setPreviousURL] = useState("");
   const pokemonStore = useSelector((state) => state.pokemons);
   const dispatch = useDispatch();
 
@@ -25,7 +25,6 @@ const IndexPage = ({ navigation }) => {
   }, []);
 
   const fetchAllPokemonsData = () => {
-    dispatch(setIsLoading(true));
     axios
       .get(pokemonURL)
       .then((response) => fetchPokemons(response))
@@ -33,8 +32,7 @@ const IndexPage = ({ navigation }) => {
   };
 
   const fetchNewPage = () => {
-    dispatch(setFetchingNewPokemonsStart());
-    console.log("before fetch: ", pokemonStore.isFetchingNewPokemons);
+    if (nextURL === null || nextURL === undefined) return;
 
     axios
       .get(nextURL)
@@ -42,9 +40,23 @@ const IndexPage = ({ navigation }) => {
       .catch((error) => console.log("Error: ", error));
   };
 
+  const fetchPreviousPage = () => {
+    if (previousURL === null || previousURL === undefined) return;
+
+    axios
+      .get(previousURL)
+      .then((response) => fetchPokemons(response))
+      .catch((error) => console.log("Error: ", error));
+  };
+
   const fetchPokemons = (response) => {
     let pokemonsArr = response.data.results;
+
     setNextURL(response.data.next);
+    setPreviousURL(response.data.previous);
+
+    dispatch(cleanPokemons());
+    dispatch(setIsLoading(true));
 
     for (let index = 0; index < pokemonsArr.length; index++) {
       axios
@@ -52,22 +64,39 @@ const IndexPage = ({ navigation }) => {
         .then((response) => dispatch(addPokemonToStore(response.data)))
         .catch((error) => console.log("Error: ", error));
     }
-
     dispatch(setIsLoading(false));
-    dispatch(setFetchingNewPokemonsEnded());
   };
 
   return (
     <View style={styles.container}>
       <View style={styles.list}>
-        {pokemonStore.isLoading ? (
+        {pokemonStore.allPokemons.length !== 25 ? (
           <Text>Is Loading</Text>
         ) : (
-          <PokemonList
-            pokemons={pokemonStore.allPokemons}
-            navigation={navigation}
-            fetchNewPage={fetchNewPage}
-          />
+          <View>
+            <PokemonList
+              pokemons={pokemonStore.allPokemons}
+              navigation={navigation}
+              fetchNewPage={fetchNewPage}
+              fetchPreviousPage={fetchPreviousPage}
+            />
+            <View style={styles.buttonsContainer}>
+              <Button
+                onPress={fetchPreviousPage}
+                title="Previous"
+                color={AppStyles.colors.listItemColor}
+                accessibilityLabel="Fetch previous page"
+                disabled={previousURL === null || previousURL === undefined}
+              />
+              <Button
+                onPress={fetchNewPage}
+                title="Next"
+                color={AppStyles.colors.listItemColor}
+                accessibilityLabel="Fetch next page"
+                disabled={nextURL === null || nextURL === undefined}
+              />
+            </View>
+          </View>
         )}
       </View>
     </View>
@@ -83,6 +112,14 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: "center",
     justifyContent: "center",
+  },
+  buttonsContainer: {
+    backgroundColor: AppStyles.colors.headerColor,
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-evenly",
+    width: "100%",
+    height: 50,
   },
 });
 
