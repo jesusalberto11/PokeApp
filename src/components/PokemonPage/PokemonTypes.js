88aace450c0b0031a95fbc9a0913f39c9375dbe1
Jesus Morales <jesusalbertomoralesrico@gmail.com>
2022-07-28 19:09:30 -0500

    Refractor: Udpdate type styles

--- src/components/PokemonPage/PokemonTypes.js
+++ src/components/PokemonPage/PokemonTypes.js
@@ -2,20 +2,51 @@ import React from "react";
 import { View, StyleSheet, Text } from "react-native";
 
 const PokemonTypes = ({ types }) => {
+  const typeWithCapitalizedFirstLetter = (name) => {
+    return <Text>{name.charAt(0).toUpperCase() + name.slice(1)}</Text>;
+  };
   return (
     <View style={styles.typesContainer}>
-      {types.map((item, id) => {
-        return <Text key={id}>Types: {item.type.name}</Text>;
-      })}
+      <Text style={styles.title}>TYPES</Text>
+      <View style={styles.types}>
+        {types.map((item, id) => {
+          return (
+            <View key={id} style={styles.button}>
+              {typeWithCapitalizedFirstLetter(item.type.name)}
+            </View>
+          );
+        })}
+      </View>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
   typesContainer: {
+    flexDirection: "column",
+    alignItems: "center",
+    justifyContent: "center",
+    padding: 5,
+  },
+  title: {
+    fontSize: 22,
+    fontWeight: "bold",
+    textAlign: "center",
+  },
+  types: {
     flexDirection: "row",
     justifyContent: "space-evenly",
-    backgroundColor: "blue",
+    width: "100%",
+    flexWrap: "wrap",
+    padding: 5,
+  },
+  button: {
+    alignItems: "center",
+    backgroundColor: "#A1CFB5",
+    borderWidth: 1,
+    borderRadius: 50,
+    padding: 5,
+    width: 100,
   },
 });
 
