6c675fe6f46a799f97c06d5854d57fc651c4b0a5
Jesus Morales <jesusalbertomoralesrico@gmail.com>
2022-07-28 21:00:50 -0500

    Refractor: Updated list buttons styles

--- src/components/PokemonList/ListButtons.js
+++ src/components/PokemonList/ListButtons.js
@@ -1,5 +1,12 @@
 import React from "react";
-import { StyleSheet, Button, View } from "react-native";
+import {
+  StyleSheet,
+  Button,
+  View,
+  TouchableOpacity,
+  Text,
+  Pressable,
+} from "react-native";
 import { AppStyles } from "../../config/Styles";
 
 const ListButtons = ({
@@ -10,20 +17,61 @@ const ListButtons = ({
 }) => {
   return (
     <View style={styles.ListButtonsContainer}>
-      <Button
-        onPress={fetchPreviousPage}
-        title="Previous"
-        color={AppStyles.colors.listItemColor}
-        accessibilityLabel="Fetch previous page"
+      <TouchableOpacity
+        style={[
+          styles.button,
+          {
+            opacity:
+              previousURL === null || previousURL === undefined ? 0.7 : 1,
+            backgroundColor:
+              previousURL === null || previousURL === undefined
+                ? "#808080"
+                : AppStyles.colors.listItemColor,
+          },
+        ]}
         disabled={previousURL === null || previousURL === undefined}
-      />
-      <Button
+        onPress={fetchPreviousPage}
+      >
+        <Text
+          style={[
+            styles.text,
+            {
+              color:
+                previousURL === null || previousURL === undefined
+                  ? "#202020"
+                  : "#fff",
+            },
+          ]}
+        >
+          PREVIOUS
+        </Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity
+        style={[
+          styles.button,
+          {
+            opacity: nextURL === null || nextURL === undefined ? 0.7 : 1,
+            backgroundColor:
+              nextURL === null || nextURL === undefined
+                ? "#808080"
+                : AppStyles.colors.listItemColor,
+          },
+        ]}
         onPress={fetchNewPage}
-        title="Next"
-        color={AppStyles.colors.listItemColor}
-        accessibilityLabel="Fetch next page"
-        disabled={nextURL === null || nextURL === undefined}
-      />
+      >
+        <Text
+          style={[
+            styles.text,
+            {
+              color:
+                nextURL === null || nextURL === undefined ? "#202020" : "#fff",
+            },
+          ]}
+        >
+          NEXT
+        </Text>
+      </TouchableOpacity>
     </View>
   );
 };
@@ -36,6 +84,17 @@ const styles = StyleSheet.create({
     justifyContent: "space-evenly",
     width: "100%",
     height: 50,
+  },
+  button: {
+    alignItems: "center",
+
+    padding: 10,
+    width: 100,
+    borderWidth: 1,
+    borderRadius: 10,
+  },
+  text: {
+    fontWeight: "bold",
   },
 });
 
